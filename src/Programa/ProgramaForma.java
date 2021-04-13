ccb11d134e6770b4d1eb0cd7d217e67db2de4961
maicongaldino <maicongacunha@gmail.com>
2021-04-13 15:02:00 -0300

    Primeiro commit

--- src/Programa/ProgramaForma.java
+++ src/Programa/ProgramaForma.java
@@ -18,7 +18,7 @@ public class ProgramaForma {
         {
             System.out.println("1 -- Calcular área circulo");
             System.out.println("2 -- Calcular área quadrado");
-            System.out.println("0 -- Encerrar programa");
+            System.out.println("0 -- Encerrar programa\n");
             opcao = ler.nextInt();
 
             switch (opcao)
@@ -27,16 +27,16 @@ public class ProgramaForma {
                     System.out.println("Encerrando o programa...");
                 break;
                 case 1:
-                    System.out.printf("Digite o raio do circulo:   ");
+                    System.out.printf("\nDigite o raio do circulo:\t");
                     circulo.setRaio(ler.nextDouble());
 
                     System.out.printf("Área do circulo: %.1f Cm²\n\n",circulo.calcularArea());
                     System.out.println(circulo.explicarCalculo() + "\n");
                 break;
                 case 2:
-                    System.out.println("Digite a altura do quadrado:   ");
+                    System.out.println("\nDigite a altura do quadrado:\t");
                     quadrado.setAltura(ler.nextDouble());
-                    System.out.println("Digite a base do quadrado:   ");
+                    System.out.println("Digite a base do quadrado:\t");
                     quadrado.setBase(ler.nextDouble());
 
                     System.out.printf("Área do quadrado: %.1f Cm²\n\n", quadrado.calcularArea());
